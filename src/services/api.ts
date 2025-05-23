e2c45d4e492d3d28812258e8592ec5dc06c7d9e1
devstar11 <devstar1221@gmail.com>
2025-05-23 17:04:42 -0500

    fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Note, User, AuthResponse, NoteFormData } from '@/types';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://todo-back-d157.onrender.com';
+const API_URL = process.env.API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
